737e206535a7b3ae72a8f007e7db31888eb05d8e
Gregfanyan <grigorfanyan@gmail.com>
2021-04-23 22:17:14 +0200

    feat: added basic structure

---
+++ src/components/SideBarForm/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function SideBarForm() {
+    return (
+        <div>
+            SideBarForm
+        </div>
+    )
+}
+
+export default SideBarForm
---
+++ src/components/Todo/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Todo() {
+    return (
+        <div>
+            Todo
+        </div>
+    )
+}
+
+export default Todo
